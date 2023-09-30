fe3c20bf5a0fd5ff90cae9deee4acda187832306
joseph-cchang <98497918+joseph-cchang@users.noreply.github.com>
2023-09-29 20:17:58 -0700

    Update Data200Assignment1ChangJ.py

--- Data200Assignment1ChangJ.py
+++ Data200Assignment1ChangJ.py
@@ -3,38 +3,37 @@
 
 # In[1]:
 
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Fish.csv')
+fish_df = pd.read_csv('Fish.csv')
+toy_df = pd.read_csv("toy_dataset.csv")
 
-st.write("Here is a display of the fish dataframe:")
-st.dataframe(df)
+st.markdown("""<style>.big-font {font-size:100px !important;} </style> """, unsafe_allow_html=True)
+st.markdown('<p class="big-font">Here is a display of the fish dataframe: </p>', unsafe_allow_html=True)
+#st.write("Here is a display of the fish dataframe:")
+st.dataframe(fish_df)
 
-
-st.write("Here is a condensed dataframe")
-st.dataframe(df[['Species', 'Weight', 'Height']])
-
+if st.button("Condensed Dataframe"):
+    st.write("Here is a condensed dataframe")
+    st.dataframe(fish_df[['Species', 'Weight', 'Height']])
 
 st.write("Here is a histogram graph")
 n_bins = 10
 fig, ax = plt.subplots()
-ax.hist(df['Height'], edgecolor = "black")
+ax.hist(fish_df['Height'], edgecolor = "black")
 ax.set_title("Histogram of Height")
 ax.set_xlabel('Height Value')
 ax.set_ylabel('Count')
 
 st.pyplot(fig)
 st.write("This histogram shows the height of the fish. The most common fish height is between the 5.0 - 7.5, with around 40 counts. Overall, the distribution seems skew right.")
-st.button("Click me")
-st.selectbox("Pick one", ["cats", "dogs"])
 
 
 st.write("Here is a scatterplot graph")
 fig, ax = plt.subplots()
-ax.scatter(df['Height'], df['Weight'])
+ax.scatter(fish_df['Height'], fish_df['Weight'])
 ax.set_xlabel("Height")
 ax.set_ylabel("Weight")
 ax.set_title("Weight vs Height")
@@ -42,15 +41,6 @@ st.pyplot(fig)
 
 st.write("This scatterplot is harder to infer, but a exponential graph could be used to infer. After 7.5 in Height, it's more difficult to access.")
 
-
-st.markdown("""
-<style>
-.big-font {
-    font-size:300px !important;
-}
-</style>
-""", unsafe_allow_html=True)
-st.markdown('<p class="big-font">Hello World !!</p>', unsafe_allow_html=True)
 
 
 
